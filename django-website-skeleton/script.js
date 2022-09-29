3e178ce241526c289e00d4b2316a1199284230c7
Danish <33805784+iDanishDev@users.noreply.github.com>
2022-09-29 11:02:19 +0530

    Commit for Minor Modification

--- django-website-skeleton/script.js
+++ django-website-skeleton/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    $(".ham-ico").click(function(){
+    $(".ham-ico").hover(function(){
         $(".bar2").css("opacity", "0");
         $(".bar1").css("transform", "translate(0, 11px) rotate(-45deg)");
         $(".bar3").css("transform", "translate(0, -11px) rotate(45deg)");
+    }, function(){
+        $(".bar2").css("opacity", "100");
+        $(".bar1").css("transform", "translate(0, -11px) rotate(0deg)");
+        $(".bar3").css("transform", "translate(0, 11px) rotate(-0deg)");
     });
 });
