6adafa73f7d5129e4dcd3f7888620f10ba2cf873
sohampatil14 <sohampatil1412@gmail.com>
2022-11-14 21:37:34 +0530

    Signup as a user with Django
    Added functionality of creating an user through signup in Django Admin

--- template_and_routing/basic/website/views.py
+++ template_and_routing/basic/website/views.py
@@ -4,12 +4,16 @@ from .models import connection
 from .forms import connectionForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
 # from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return render(request, 'index.html', {})
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('chat')
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -18,8 +22,11 @@ def login_user(request):
             login(request, user)
             request.session['name'] = username
             request.session['profile_photo'] = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_03.jpg"
+            request.session['is_login'] = True
             messages.success(request, "Login Sucessful")
-            return redirect('chat')
+            redirect ('chat')
+            # all_connections = connection.objects.all
+            # return render(request, 'chat.html', { 'connections': all_connections })
         else:
             return render(request, 'login.html', {})
     else:
@@ -27,31 +34,28 @@ def login_user(request):
 
 # @csrf_exempt
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('chat')
     if request.method == "POST":
-        form = connectionForm(request.POST or None)
-        if form.is_valid():
-            form.save()
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['name'], '', request.POST['password1'])
+            user.save()
+            return redirect('login')
         else:
-            name = request.POST['name']
-            password = request.POST['password']
-            profile_photo = request.POST['profile_photo']
-            messages.error(request, "Sign Up Unsucessful")
-            return render(request, 'signup.html', {
-                'name':name, 
-                'password':password, 
-                'profile_photo':profile_photo,
-                })
-        messages.success(request, "Sign Up Sucessful")
-        return redirect('login')
-
+            messages.error(request, "Signup Unsucessful)
+            return render(request, 'signup.html', {})
     else:
         return render(request, 'signup.html', {})
 
 def chat(request):
+    if ~request.user.is_authenticated:
+        return redirect('login')
     all_connections = connection.objects.all
     return render(request, 'chat.html', { 'connections': all_connections })
 
 def video(request):
+    if ~request.user.is_authenticated:
+        return redirect('login')
     all_connections = connection.objects.all
     return render(request, 'video.html', { 'connections': all_connections })
 
