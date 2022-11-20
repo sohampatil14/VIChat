c9f22b525921bbc9a3933a0ad3fdb6686f0eb40c
sohampatil14 <sohampatil1412@gmail.com>
2022-11-20 23:03:37 +0530

    Added a view profile page and some UI updates
    A totally new profile page, added logout feature, now user can view error message while signup and login and a static view of how sent and received messages will be shown on chat area. Also fixed ERR_TOO_MANY_REDIRECTS error.

--- template_and_routing/basic/website/urls.py
+++ template_and_routing/basic/website/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = "home"),
-    path('login/', views.login_user, name = "login"),
-    path('signup/', views.signup, name = "signup"),
-    path('chat/', views.chat, name = "chat"),
-    path('video/', views.video, name = "video"),
+    path('', views.home, name="home"),
+    path('login/', views.login_user, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('chat/', views.chat, name="chat"),
+    path('video/', views.video, name="video"),
+    path('logout/', views.logout_user, name="logout"),
+    path('profile/', views.profile, name="profile"),
     # path('chat/0/', views.chat_clicked, name = "chat_clicked"),
 ]
--- template_and_routing/basic/website/views.py
+++ template_and_routing/basic/website/views.py
@@ -6,11 +6,15 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 # from django.contrib.auth.forms import UserCreationForm
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
+# from django.urls import reverse, reverse_lazy
+
 
 def home(request):
     return render(request, 'index.html', {})
 
+
+@csrf_exempt
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('chat')
@@ -24,40 +28,73 @@ def login_user(request):
             request.session['profile_photo'] = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_03.jpg"
             request.session['is_login'] = True
             messages.success(request, "Login Sucessful")
-            redirect ('chat')
-            # all_connections = connection.objects.all
-            # return render(request, 'chat.html', { 'connections': all_connections })
+            # return redirect ('chat')
+            all_connections = connection.objects.all
+            return render(request, 'chat.html', {'connections': all_connections})
         else:
+            messages.error(request, "Invalid Credentials")
             return render(request, 'login.html', {})
     else:
         return render(request, 'login.html', {})
 
-# @csrf_exempt
+
+@csrf_exempt
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout Sucessful")
+    return redirect('login')
+
+
+@csrf_exempt
+def profile(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        photo = request.POST["photo"]
+        return render(request, 'profile.html', {'name': name, 'photo': photo})
+
+
+@csrf_exempt
 def signup(request):
     if request.user.is_authenticated:
         return redirect('chat')
-    if request.method == "POST":
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['name'], '', request.POST['password1'])
-            user.save()
-            return redirect('login')
-        else:
-            messages.error(request, "Signup Unsucessful)
-            return render(request, 'signup.html', {})
     else:
-        return render(request, 'signup.html', {})
+        if request.method == "POST":
+            if request.POST['password1'] == request.POST['password2']:
+                user = User.objects.create_user(
+                    request.POST['name'], '', request.POST['password1'])
+                user.save()
+                messages.success(
+                    request, "Signup Sucessful! Please Login to continue.")
+                return render(request, 'login.html', {})
+            else:
+                messages.error(
+                    request, "Signup Unsucessful! Please try again.")
+                return render(request, 'signup.html', {})
 
+        else:
+            return render(request, 'signup.html', {})
+
+
+@csrf_exempt
 def chat(request):
-    if ~request.user.is_authenticated:
+    if request.user.is_authenticated:
+        all_connections = connection.objects.all
+        return render(request, 'chat.html', {'connections': all_connections})
+    else:
         return redirect('login')
-    all_connections = connection.objects.all
-    return render(request, 'chat.html', { 'connections': all_connections })
 
+
+@csrf_exempt
 def video(request):
-    if ~request.user.is_authenticated:
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            all_connections = connection.objects.all
+            name = request.POST["name"]
+            photo = request.POST["photo"]
+            return render(request, 'video.html', {'connections': all_connections, 'name': name, 'photo': photo})
+    else:
         return redirect('login')
-    all_connections = connection.objects.all
-    return render(request, 'video.html', { 'connections': all_connections })
+
 
 def chat_clicked(request):
     return render(request, 'done')
